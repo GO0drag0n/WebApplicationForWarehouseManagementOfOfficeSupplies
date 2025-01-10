11dee284383824eb474a904626258b6bff15ee07
konst <konst@KONSTANTIN>
2025-01-11 00:26:24 +0200

    Updated view on login and register page (incl. Media Query).

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿    using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 
 namespace WebApplicationForWarehouseManagementOfOfficeSupplies.Models
